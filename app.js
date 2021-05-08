4cb2edcd91daa3f7834e8c25d43509fe6ac49976
Arslan <arslanbutt7545@gmail.com>
2021-05-08 10:47:57 +0500

    Add encryption-key to env, level 2 completed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -21,8 +22,8 @@ const userSchema =new mongoose.Schema ({
   password : String
 });
 
-const secret = "Thisisthesecretstringforschema.";
-userSchema.plugin(encrypt,{secret: secret, excludeFromEncryption: ['email'] });
+
+userSchema.plugin(encrypt,{secret: process.env.SECRET, excludeFromEncryption: ['email'] });
 
 const User = new mongoose.model("User",userSchema);
 
